b83794761e34adc78cd254645a57a15abfb19ffc
sss-0916 <sss-0916@qq.com>
2020-05-19 09:37:39 +0800

    末尾0的个数

---
+++ 17.末尾0的个数.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        int cnt = 0;
+        while (num != 0) {
+            cnt += num / 5;
+            num /= 5;
+        }
+        
+        System.out.println(cnt);
+    }
+}
